84d6b298c61034cc1e4e89a1b1029319fde42419
Zeke Davis <edavis@javafoundry.com>
2024-07-09 09:42:33 -0400

    Through A/B test

--- FromScratch/DSFS/inference.py
+++ FromScratch/DSFS/inference.py
@@ -1,17 +1,76 @@
 from typing import Tuple
-from probability import *
+
+try:
+    from .probability import *
+except:
+    from probability import *
+    
 import math
 
 normal_probability_below = normal_cdf
 
+def normal_approximation_to_binomial(n: int, p: float) -> Tuple[float, float]:
+    """Returns mu and sigma corresponding to a Binomial(n, p)"""
+    
+    mu = p * n
+    sigma = math.sqrt(p * (1 - p) * n)
+    return mu, sigma
+    
 def normal_probability_above(lo: float, mu: float = 0, sigma: float = 1) -> float:
+    """The probability that an N(mu, sigma) is greater than lo."""
+    
     return 1 - normal_cdf(lo, mu, sigma)
 
 def normal_probability_between(lo: float, hi: float, mu: float = 0, sigma: float = 1) -> float:
+    """The probability that an N(mu, sigma) is between lo and hi."""
+    
     return normal_cdf(hi, mu, sigma) - normal_cdf(lo, mu, sigma)
 
 def normal_probability_outside(lo: float, hi: float, mu: float = 0, sigma: float = 1) -> float:
+    """The probability that an N(mu, sigma) is not between lo and hi."""
+    
     return 1 - normal_probability_between(lo, hi, mu, sigma)
 
 def normal_upper_bound(probability: float, mu: float = 0, sigma: float = 1) -> float:
+    """Returns the z for which P(Z <= z) = probability"""
+    
     return inverse_normal_cdf(probability, mu, sigma)
+
+def normal_lower_bound(probability: float, mu: float = 0, sigma: float = 1) -> float:
+    """Returns the z for which P(Z >= z) = probability"""
+    
+    return inverse_normal_cdf(1 - probability, mu, sigma)
+
+def normal_two_sided_bounds(probability: float, mu: float = 0, sigma: float = 1) -> Tuple[float, float]:
+    """Returns the symmetric (about the mean) bounds that contain the specified probability"""
+    
+    tail_probability = (1 - probability) / 2
+    
+    upper_bound = normal_lower_bound(tail_probability, mu, sigma)
+    
+    lower_bound = normal_upper_bound(tail_probability, mu, sigma)
+    
+    return lower_bound, upper_bound
+
+def two_sided_p_value(x: float, mu: float = 0, sigma: float = 1) -> float:
+    """How likely are we to see a value at least as extreme as x (in either direction) if our values are from an N(mu, sigma)?"""
+    if x >= mu:
+        # x is greater than the mean, so the tail is everything greater than x
+        return 2 * normal_probability_above(x, mu, sigma)
+
+    else:
+        # x is less than the mean, so the tail is everything less than x
+        return 2 * normal_probability_below(x, mu, sigma)
+    
+def estimated_parameters(N: int, n: int) -> Tuple[float, float]:
+    p = n/N
+    sigma = math.sqrt(p * (1 - p) / N)
+    return p, sigma
+
+def a_b_test_statistic(N_A: int, n_A: int, N_B: int, n_B: int) -> float:
+    p_A, sigma_A = estimated_parameters(N_A, n_A)
+    p_B, sigma_B = estimated_parameters(N_B, n_B)
+    return (p_B - p_A) / math.sqrt(sigma_A ** 2 + sigma_B ** 2)
+
+
+        

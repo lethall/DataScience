329d1c89c4c9dea8c01173c3ec305b630d880df3
Zeke Davis <edavis@javafoundry.com>
2024-07-18 09:32:29 -0400

    Progressing through Gradient Descent

--- FromScratch/DSFS/gradient_decent.py
+++ FromScratch/DSFS/gradient_decent.py
@@ -9,3 +9,19 @@ except:
 
 def difference_quotient(f: Callable[[float], float], x: float, h: float) -> float:
     return (f(x + h) - f(x)) / h
+
+def square(x: float) -> float:
+    return x * x
+
+def derivative(x: float) -> float:
+    return 2 * x
+
+def partial_difference_quotient(f: Callable[[Vector], float], v: Vector, i: int, h: float) -> float:
+    """Returns the i-th partial difference quotient of f at v"""
+    
+    w = [v_j + (h if j == i else 0) for j, v_j in enumerate(v)]
+    
+    return (f(w) - f(v)) / h
+
+def estimates_gradient(f: Callable[[Vector], float], v: Vector, h: float = 0.0001):
+    return [partial_difference_quotient(f, v, i, h) for i in range(len(v))]

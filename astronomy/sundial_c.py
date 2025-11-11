58804a48f94b3444b385dad0dab847abfee0c451
Lee Hall <javahall@gmail.com>
2025-11-11 11:25:52 -0500

    Times and organization

---
+++ astronomy/sundial_c.py
@@ -0,0 +1,44 @@
+from skyfield.api import load, N, W, wgs84, utc, Angle
+from skyfield import almanac
+from datetime import datetime
+from pytz import timezone
+from sys import argv, exit
+import os
+
+print(f"prog: '{argv[0]}' cwd: {os.curdir}")
+
+"""
+"""
+est = timezone("US/Eastern")
+ts = load.timescale()
+de421 = load("de421.bsp")
+earth, sun, moon = de421["earth"], de421["sun"], de421["moon"]
+# 40°37′55″ N  82°58′37″ W
+caledonia_wsg = wgs84.latlon(40.63194 * N, 82.9769 * W)
+caledonia = earth + caledonia_wsg
+
+def print_observe(from_obj, obj, tt):
+    pos = from_obj.at(tt).observe(obj)
+    alt, az, d = pos.apparent().altaz()
+    ra, dec, d = pos.radec()
+    print(f"{obj.target_name.split()[-1]}\nalt: {alt.degrees:.2f}\t az: {az.degrees:.1f}\n ra: {ra.hours:.3f}h\tdec: {dec.degrees:.1f}")
+
+a = ts.from_datetime(datetime.strptime("2025-11-03 00:00 UTC", "%Y-%m-%d %H:%M %Z").replace(tzinfo=utc))
+b = ts.from_datetime(datetime.strptime("2025-11-16 00:00 UTC", "%Y-%m-%d %H:%M %Z").replace(tzinfo=utc))
+t, y = almanac.find_discrete(a, b, almanac.moon_phases(de421))
+
+for tm, ty, ph in zip(t.astimezone(est),y,[almanac.MOON_PHASES[yi] for yi in y]):
+    print(f"{tm.isoformat(' ', 'seconds')} {ph}")
+# almanac.moon_phases([a,b])
+rt, y = almanac.find_risings(caledonia, sun, a, b)
+st, y = almanac.find_settings(caledonia, sun, a, b)
+for rtm, stm in zip(rt,st):
+    print(f"{rtm.astimezone(est).date()} {rtm.astimezone(est).time().isoformat('seconds')} / {stm.astimezone(est).time().isoformat('seconds')}")
+
+t = ts.now()
+#t = ts.from_datetime(datetime.strptime("2025-08-23 06:06 UTC", "%Y-%m-%d %H:%M %Z").replace(tzinfo=utc))
+lst = caledonia_wsg.lst_hours_at(t)
+print(f"{t.tt_strftime()}\nLST: {Angle(hours=lst)} ({lst:.2f}h)")
+print_observe(caledonia, sun, t)
+print_observe(caledonia, moon, t)
+
